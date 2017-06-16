eecbc86ca00d46807e6ebb9905af660fa44493d4
Ton Pham <tonpham35@gmail.com>
2017-06-16 15:33:38 +0800

    updated EXIF gem in eventphoto for when there is EXIF data

--- app/models/eventphoto.rb
+++ app/models/eventphoto.rb
@@ -3,17 +3,36 @@ class Eventphoto < ApplicationRecord
 	mount_uploader :photo, ImageUploader
 
 	def self.datetime(img_file)
-		data = Exif::Data.new(img_file)
-		return datetime = data.date_time	
+		if Exif::Data.new(img_file).date_time != nil
+			data = Exif::Data.new(img_file)
+			datetime = data.date_time
+		else
+			datetime = nil
+		end
+
+		return datetime	
 	end
 
 	def self.latitude(img_file)
-		data = Exif::Data.new(img_file)
-		return datetime = data.gps_latitude	
+		if Exif::Data.new(img_file).gps_latitude != nil
+			data = Exif::Data.new(img_file)
+			latitude = data.gps_latitude	
+		else
+			latitude = nil
+		end
+		
+		return latitude
 	end
 
 	def self.longitude(img_file)
-		data = Exif::Data.new(img_file)
-		return datetime = data.gps_longitude
+		if Exif::Data.new(img_file).gps_longitude != nil
+			data = Exif::Data.new(img_file)
+			longitude = data.gps_longitude
+		else
+			longitude = nil
+		end
+
+		return longitude
 	end	
+
 end
