1accef115f1568b5083f1d3398a70f5ccf65a0f2
Jacy <Jacyang93@gmail.com>
2017-06-19 17:26:33 +0800

    can add participant

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,7 @@ class User < ApplicationRecord
 	has_many :friends, :through => :friendships
 	has_many :inverse_friendships, :class_name => "Friendship", :foreign_key => "friend_id"
 	has_many :inverse_friends, :through => :inverse_friendships, :source => :user
-
+	has_many :story_members
 # In app/models/user.rb
 	class User < ApplicationRecord
 	    has_many :authentications, dependent: :destroy
