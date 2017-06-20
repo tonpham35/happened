7125d16a90ebd565be48300b86718cb147f068b5
Ton Pham <tonpham35@gmail.com>
2017-06-20 12:12:37 +0800

    updated maps cards

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -23,7 +23,7 @@ class EventsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def event_params
-      params.require(:event).permit(:type_of_event, :what, :why, :when, :where, :mode_of_transport, :flight_num, :flight_time, :flight_date, :accomodation_type, :accomodation_address, :accomodation_checkin, :accomodation_checkout, :story_id, :location_longitude, :location_latitude, eventphotos_attributes: [:photo])
+      params.require(:event).permit(:type_of_event, :what, :why, :when, :where, :mode_of_transport, :flight_num, :flight_time, :flight_date, :accomodation_type, :accomodation_address, :accomodation_checkin, :accomodation_checkout, :story_id, :long, :lat, eventphotos_attributes: [:photo])
     end
 
 end
